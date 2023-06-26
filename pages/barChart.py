6ac7c107255951afa42aaab8a176cfbc8985a19b
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-27 02:11:27 +0900

    update

--- pages/barChart.py
+++ pages/barChart.py
@@ -1,12 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/aaingyunii/230627-28_Team_project/main/%EC%9D%91%EA%B8%89%EC%9D%98%EB%A3%8C%EA%B8%B0%EA%B4%80%EB%B0%8F%EC%9D%91%EA%B8%89%EC%9D%98%EB%A3%8C%EC%A7%80%EC%9B%90%EC%84%BC%ED%84%B0%ED%98%84%ED%99%A9.csv",
     encoding='cp949'
 )
-st.bar_chart(df)
+
+
+# 바 차트 생성
+fig = px.bar(df, x='시도', y='의료기관수')
+
+# Streamlit 앱에 바 차트 표시
+st.plotly_chart(fig)
+
+
 # # 시도별 의료기관 수 계산
 # hospital_count = df['시군명'].value_counts()
 
