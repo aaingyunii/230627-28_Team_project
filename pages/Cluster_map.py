2362a95467d7c4e16b3c30c3a651e9becd67d272
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-27 15:21:22 +0900

    change st_folium

--- pages/Cluster_map.py
+++ pages/Cluster_map.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import folium
-import streamlit-folium 
+from streamlit_folium import st_folium
 from folium import plugins
 import common
 ​
@@ -66,4 +66,4 @@ st.sidebar.title("시군별 병원 및 의료센터")
 st.sidebar.markdown("* 시군별 병원 및 의료센터 숫자와 위치를 알 수 있습니다.  \n* 위치를 클릭하면, 병원 이름과 대표 번호 및 특수 센터 보유 여부를 알 수 있습니다.")
 ​
 # 지도 출력
-streamlit-folium(m)
+st_folium(m)
