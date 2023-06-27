66ef59767aee05ddb72f0dc570b1853c52610e84
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-27 17:17:26 +0900

    add title

--- pages/mapVisual.py
+++ pages/mapVisual.py
@@ -2,12 +2,13 @@ import streamlit as st
 from streamlit_folium import st_folium
 import folium
 import pandas as pd
+import common
 
+common.page_config()
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/aaingyunii/230627-28_Team_project/main/%EC%9D%91%EA%B8%89%EC%9D%98%EB%A3%8C%EA%B8%B0%EA%B4%80%EB%B0%8F%EC%9D%91%EA%B8%89%EC%9D%98%EB%A3%8C%EC%A7%80%EC%9B%90%EC%84%BC%ED%84%B0%ED%98%84%ED%99%A9.csv",
-    encoding='cp949'
-)
+st.title("기본 지도 공간 시각화")
+
+df = common.get_data()
 
 # 서울-경기도 부근 지도 생성
 m = folium.Map(location=[37.5665, 126.9780], zoom_start=10)
