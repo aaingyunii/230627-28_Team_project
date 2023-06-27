ea85e0814bb72803e934083ade29908d431c0994
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-28 01:18:12 +0900

    add comment

--- pages/Scatter_plot.py
+++ pages/Scatter_plot.py
@@ -1,64 +1,49 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import common
+import streamlit as st 
+import matplotlib.pyplot as plt  # Matplotlib: 데이터 시각화를 위한 라이브러리
+import plotly.graph_objects as go  # Plotly: 인터랙티브 시각화를 위한 라이브러리
+import common  
+import matplotlib.font_manager as fm  # 한글 폰트 적용을 위한 라이브러리 가져오기
 
-# 한글 폰트 적용
-import matplotlib.font_manager as fm
+# common 파일에서 정의된 웹 페이지 탭 꾸미기 함수 호출
+common.page_config() 
 
-# common 파일을 통해 웹 페이지 탭 꾸미기
-common.page_config()
-
-
-st.title("산점도")
+st.title("산점도") 
 
 # 한글 폰트 설정
-font_path = './NanumGothic.ttf'  # 한글 폰트 파일 경로.
+font_path = './NanumGothic.ttf'  # 한글 폰트 파일 경로
 fontprop = fm.FontProperties(fname=font_path)
-plt.rc('font', family=fontprop.get_name())
+plt.rc('font', family=fontprop.get_name())  # 한글 폰트 설정
 
-# common 파일을 통해 데이터프레임 불러오기
-df = common.get_data()
+# common 파일에서 정의된 데이터프레임을 가져오는 함수 호출
+df = common.get_data()  
 
-# 챗지피한테 물어보기
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# tab으로 나누기
-tab1, tab2 = st.tabs(["Plotly", "Pyplot"])
+# 경고 메시지 미출력을 위한 설정
+st.set_option('deprecation.showPyplotGlobalUse', False)  
 
-# plotly로 산점도 그래프 만들기
+
+tab1, tab2 = st.tabs(["Plotly", "Pyplot"])  
+# Plotly로 산점도 그래프 만들기
 with tab1:
-    fig = go.Figure(data=[go.Scatter(x=df['경도'], y=df['위도'], mode='markers')])
+    fig = go.Figure(data=[go.Scatter(x=df['경도'], y=df['위도'], mode='markers')])  # Scatter 객체를 사용하여 산점도 그래프 생성
     fig.update_layout(
         xaxis=dict(
-            title='시도',
+            title='경도',
             tickangle=90,
         ),
         yaxis=dict(
-            title='의료기관 수',
+            title='위도',
         ),
-        title='시도별 의료기관 수',
+        title='소재지 위도와 경도를 이용한 산점도',
     )
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)  # Plotly 그래프를 Streamlit에 표시
 
 
-# pyplot으로 산점도 그래프 만들기
+# Pyplot으로 산점도 그래프 만들기
 with tab2:
-    plt.scatter(df['경도'],df['위도'])
-    plt.xlabel('경도', fontproperties=fontprop)  # 한글 폰트 설정
-    plt.ylabel('위도', fontproperties=fontprop)  # 한글 폰트 설정
-    plt.title('소재지 위도와 경도를 이용한 산점도', fontproperties=fontprop)  # 한글 폰트 설정
-    plt.xticks(rotation=90, fontproperties=fontprop)  # 한글 폰트 설정
-    st.pyplot()
-
-
-
-
-
-# 소재지 위도와 경도를 사용하여 산점도 그리기
-plt.figure(figsize=(10, 6))
-plt.scatter(df['경도'], df['위도'])
-plt.xlabel('경도')
-plt.ylabel('위도')
-plt.title('병원 위치 분포')
-plt.show()
+    plt.scatter(df['경도'], df['위도'])  # scatter 함수를 사용하여 산점도 그래프 생성
+    plt.xlabel('경도', fontproperties=fontprop)  # x축 레이블 설정 (한글 폰트 적용)
+    plt.ylabel('위도', fontproperties=fontprop)  # y축 레이블 설정 (한글 폰트 적용)
+    plt.title('소재지 위도와 경도를 이용한 산점도', fontproperties=fontprop)  # 그래프 제목 설정 (한글 폰트 적용)
+    plt.xticks(rotation=90, fontproperties=fontprop)  # x축 눈금 레이블 회전 및 한글 폰트 설정
+    st.pyplot()  
--- pages/mapVisual.py
+++ pages/mapVisual.py
@@ -1,16 +1,20 @@
-import streamlit as st
-from streamlit_folium import st_folium
-import folium
-import pandas as pd
-import common
+# 필요한 라이브러리 가져오기
+import streamlit as st 
+from streamlit_folium import st_folium  # Streamlit-Folium: Streamlit에 Folium 지도 표시를 위한 라이브러리
+import folium  # Folium: 인터랙티브 지도 생성을 위한 라이브러리
+import pandas as pd  
+import common 
 
-common.page_config()
+# common 파일에서 정의된 웹 페이지 탭 꾸미기 함수 호출
+common.page_config()  
 
-st.title("기본 지도 공간 시각화")
+st.title("기본 지도 공간 시각화") 
 
-@st.cache_data(experimental_allow_widgets=True)
+# 데이터 캐싱을 위한 데코레이터 설정
+@st.cache_data(experimental_allow_widgets=True)  
 def load_map():
-    df = common.get_data()
+    # common 파일에서 정의된 데이터프레임을 가져오는 함수 호출
+    df = common.get_data() 
 
     # 서울-경기도 부근 지도 생성
     m = folium.Map(location=[37.5665, 126.9780], zoom_start=10)
@@ -19,12 +23,13 @@ def load_map():
     for index, row in df.iterrows():
         if pd.notnull(row['위도']) and pd.notnull(row['경도']):
             lat, lon = row['위도'], row['경도']
+
             # 서울-경기도 부근인 경우에만 표시
             if 37.0 <= lat <= 38.5 and 126.0 <= lon <= 127.5:
-                folium.Marker(location=[lat, lon]).add_to(m)
+                folium.Marker(location=[lat, lon]).add_to(m)  # Folium의 Marker 객체를 사용하여 위치 표시
 
-    # 지도 출력
-    st_folium(m)
+    # Streamlit-Folium을 사용하여 Folium 지도를 Streamlit에 표시
+    st_folium(m) 
 
 if __name__ == "__main__":
-    load_map()
+    load_map()  # load_map 함수 호출하여 지도 출력
