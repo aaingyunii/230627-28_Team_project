0442850bb19c5a1b13c396bdfa4e567e513858dc
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-27 12:33:34 +0900

    encoding

--- common.py
+++ common.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @st.cache_data
 def get_data():
-    return pd.read_csv("./hospital.csv")
+    return pd.read_csv("./hospital.csv",encoding="cp949")
 
 def page_config():
     st.set_page_config(
