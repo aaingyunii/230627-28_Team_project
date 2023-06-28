347bfc4076f01f886e0b2c37694c3a590ddefe03
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-06-28 17:05:26 +0900

    add st.cache.clear_cache()

--- pages/08_Various_marker_map.py
+++ pages/08_Various_marker_map.py
@@ -5,6 +5,7 @@ import common
 
 # 응급센터 종류
 eCenter_array = ("응급의료지원센터여부", "전문응급의료센터여부", "전문응급센터전문분야", "권역외상센터여부", "지역외상센터여부")
+st.cache.clear_cache()
 
 # 응급센터가 존재하는지 체크하는 함수
 def check_option(row) :
